2009939c13cba8f91661b32953610204c30820ec
kfujita <kfujita19810518@gmail.com>
2024-04-11 09:34:19 +0900

    2.5のアプリケーション

---
+++ 2_5_zoo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"kensuke/2_5_zoo/animals"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module kensuke
+
+go 1.22.2
